2ffb5202d29a05dc0d2de29973382ada22b18e20
root <15193103821@163.com>
2021-02-15 20:02:21 +0800

    版本2

--- views.py
+++ views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse("hello world")
 
+def list_page(request):
+    return redirect('/index')
+
+
+ 
 
 
 
